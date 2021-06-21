8dff4f2ea253fd172f23434a2044cb171809ccba
Mohdan <mohdan.barzankaev@gmail.com>
2021-06-21 19:09:06 +0300

    start

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,6 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="card">
+    <h1>Как Vue работает с TypeScript?</h1>
+    <button class="btn">Открыть пользователей</button>
   </div>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
-
-export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-})
-</script>
