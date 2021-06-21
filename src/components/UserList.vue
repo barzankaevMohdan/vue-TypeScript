9d34d6ef051d14108977ad7ca176c0ff7c998641
Mohdan <mohdan.barzankaev@gmail.com>
2021-06-21 22:57:29 +0300

    suspense async ts

---
+++ src/components/UserList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="list">
+        <li
+        class="list-item"
+        v-for="user in users"
+        :key=user.id
+        >
+        {{user.name}}
+        </li>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useUsers } from '@/use/users'
+
+export default defineComponent({
+  async setup () {
+    const { users } = await useUsers()
+    return { users }
+  }
+})
+</script>
